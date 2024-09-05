fde44faafb8bf3b1b5c50a33957884cab7442254
Echchi <112493260+Echchi@users.noreply.github.com>
2024-09-05 17:24:20 +0900

    버튼 컴포넌트화

--- src/app/Layout.tsx
+++ src/app/Layout.tsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 
 const Layout = () => {
   return (
-    <div className="w-full h-screen bg-lime-600 flex *:max-w-xl">
+    <div className="w-full h-screen bg-lime-700 flex *:max-w-xl">
       <Outlet />
     </div>
   );
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -7,16 +7,22 @@ import { AnimatePresence } from "framer-motion";
 import { motion } from "framer-motion";
 import { faUser } from "@fortawesome/free-solid-svg-icons";
 import { useLocation, useNavigate } from "react-router-dom";
+import Button from "../sahred/ui/Button.tsx";
 const Login = () => {
   const navigate = useNavigate();
-  const [code, setCode] = useState("");
+  const [form, setForm] = useState({
+    code: "",
+    nickName: "",
+  });
+  const [errors, setErrors] = useState({
+    code: "",
+    nickName: "",
+  });
   const [codeCheck, setCodeCheck] = useState(false);
-  const [codeErrorMessage, setCodeErrorMessage] = useState("");
-  const [nickName, setNickName] = useState("");
-  const [nickNameErrorMessage, setnickNameErrorMessage] = useState("");
+
   const handleOnChangeCode = (value: string) => {
     if (value.length === 0) {
-      setCodeErrorMessage("");
+      setErrors((prev) => ({ ...prev, code: "" }));
       setCodeCheck(false);
     }
     debounceCode(value.trim());
@@ -24,33 +30,34 @@ const Login = () => {
 
   const debounceCode = useMemo(() => {
     return debounce((code: string) => {
-      setCode(code);
+     setForm((prev)=>({...prev,code}))
     }, 500);
   }, []);
 
   useEffect(() => {
-    if (code.length === 0) return;
-    if (code === "23899192") {
+    if (form.code.length === 0) return;
+    if (form.code === "23899192") {
       localStorage.setItem("role", "student");
       setCodeCheck(true);
-      setCodeErrorMessage("");
+      setErrors((prev) => ({ ...prev, code: "" }));
     } else {
-      setCodeErrorMessage("접속 코드를 다시 확인해주세요!");
+      setErrors((prev) => ({ ...prev, code: "접속 코드를 다시 확인해주세요!" }));
     }
-  }, [code]);
+  }, [form.code]);
 
   const handleOnClick = () => {
-    if (nickName.length > 0) {
-      localStorage.setItem("name", nickName);
+    if (form.nickName.trim().length > 0) {
+      localStorage.setItem("name", form.nickName.trim());
       navigate("/workspace");
+    } else {
+      setErrors((prev) => ({ ...prev, nickName: "이름을 입력해주세요!" }));
     }
-    setnickNameErrorMessage("이름을 입력해주세요!");
-  };
+  }
 
   return (
     <div className="min-w-fit w-1/3 min-h-fit h-1/2 m-auto bg-amber-50 rounded-lg shadow flex flex-col justify-center items-center py-5">
-      <p className="w-full text-4xl font-dunggeunmiso-b text-center text-lime-500">
-        공통 로그인 폼
+      <p className="w-full text-4xl font-dunggeunmiso-b text-center text-lime-600">
+        코딩 드론 플랫폼
       </p>
       <div className="w-2/3 space-y-3">
         <div className="w-full mt-10">
@@ -60,8 +67,8 @@ const Login = () => {
             icon={<FontAwesomeIcon icon={faLock} />}
             placeholder="접속 코드"
             onChange={(event) => handleOnChangeCode(event.target.value)}
-            value={code}
-            errorMessage={codeErrorMessage}
+            value={form.code}
+            errorMessage={errors.code}
           />
         </div>
         <AnimatePresence mode="popLayout">
@@ -79,22 +86,18 @@ const Login = () => {
                 maxLength={10}
                 icon={<FontAwesomeIcon icon={faUser} />}
                 placeholder="이름"
-                value={code}
-                onChange={(event) => setNickName(event.target.value)}
-                errorMessage={nickNameErrorMessage}
+                value={form.nickName}
+                onChange={(event) =>
+                    setForm((prev) => ({ ...prev, nickName: event.target.value }))
+                }
+                errorMessage={errors.nickName}
               />
             </motion.div>
           )}
         </AnimatePresence>
-
-        <button
-          className="!mt-6 w-full py-4 text-2xl font-dunggeunmiso-b bg-yellow-400 rounded-lg text-white disabled:bg-neutral-400 shadow-lg disabled:shadow-none hover:bg-yellow-300 hover:shadow transition-all"
-          disabled={!codeCheck || nickName.length === 0}
-          onClick={handleOnClick}
-        >
-          시작하기
-        </button>
+        <Button title={"시작하기"} onClick={handleOnClick} className={"!mt-6"} disabled={!codeCheck || form.nickName.trim().length === 0}/>
       </div>
+      <button className="">강사 로그인</button>
     </div>
   );
 };
