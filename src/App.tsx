5fdfedcb87214597e487fe09da472fe631accfdf
Echchi <112493260+Echchi@users.noreply.github.com>
2025-01-08 22:51:43 +0900

    axios, reactQuery 설치

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import { RouterProvider } from "react-router-dom";
 import Router from "./app/hocs/router/Router.tsx";
 import { RecoilRoot } from "recoil";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 function App() {
+  const queryClient = new QueryClient();
   return (
     <RecoilRoot>
-      <RouterProvider router={Router} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={Router} />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </RecoilRoot>
   );
 }
