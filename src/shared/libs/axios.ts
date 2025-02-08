554b8e7e7c55603aa971d2e64bc42bb00d776bb7
Echchi <112493260+Echchi@users.noreply.github.com>
2025-02-08 17:13:01 +0900

    강의 생성 구현

--- src/shared/libs/axios.ts
+++ src/shared/libs/axios.ts
@@ -95,7 +95,11 @@ axiosInstance.interceptors.response.use(
   async (error) => {
     const originalRequest = error.config;
 
-    if (error.response?.status === 401 && !originalRequest._retry) {
+    if (
+      originalRequest.url === "/login" &&
+      error.response?.status === 401 &&
+      !originalRequest._retry
+    ) {
       originalRequest._retry = true;
 
       try {
